b13e616298b05fd20cea0669e61875bed18ca1ea
Rizzo-419 <TylerB.Other@gmail.com>
2019-07-14 00:33:15 -0400

    Finished section 4
    -added video player

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 
+var barSize=600;
 
+function doFirst() {
+    myMovie = document.getElementById('myMovie');
+    playButton = document.getElementById('playButton');
+    bar = document.getElementById('defaultBar');
+    progressBar = document.getElementById('progressBar');
 
-//Back to Top Button
-window.addEventListener("scroll", backToTop);//Could also use window.onscroll = function() {backToTop()};
-function backToTop() {
-    if(document.documentElement.scrollTop > 375) {
-        document.getElementById("backToTop").style.visibility = "visible";
-        document.getElementById("logo-container").style.opacity = "1";   
-    }
-        else {
-            document.getElementById("backToTop").style.visibility = "hidden";
+    playButton.addEventListener('click', playOrPause, false);
+    bar.addEventListener('click', clickedBar, false);
+}
+function playOrPause(){
+    if(!myMovie.paused && !myMovie.ended) {//video is playing
+        myMovie.pause();
+        playButton.innerHTML = "Play";
+        window.clearInterval(updateBar);
+    }else{
+        myMovie.play();
+        playButton.innerHTML="Pause";
+        updateBar=setInterval(update,500);
+    }       
+}
+function update(){
+    if(!myMovie.ended){
+        var size=parseInt(myMovie.currentTime*barSize/myMovie.duration);
+        progressBar.style.width=size+"px";
+    }else{
+        progressBar.style.width="0px";
+        playButton.innerHTML = "Play";
+        window.clearInterval(updateBar);
     }
 }
+function clickedBar(e){//pasing e parameter in for mouse position
+    if(!myMovie.paused && !myMovie.ended) {
+        var mouseX = e.pageX-bar.offsetLeft*1.4;
+        var newtime=mouseX*myMovie.duration/barSize;
+        myMovie.currentTime = newtime;
+        progressBar.style.width=MouseX+'px';
+    }
+}
+window.addEventListener('load',doFirst, false);
+
+
 
 //Transform name tag on hover
 function nameLarge () {
@@ -20,3 +50,19 @@ function nameLarge () {
 function nameRegular () {
     document.getElementById("combined-tags").style.transform = "scale(1)";
 }
+
+
+
+//Back to Top Button & Logo Container visibility
+window.onscroll = function() {backToTop()};//Could also use window.addEventListener("scroll", backToTop);
+function backToTop() {
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("backToTop").style.visibility = "visible";
+        document.getElementById("logo-container").style.opacity = "1";
+    }
+        else {
+            document.getElementById("backToTop").style.visibility = "hidden";
+    }
+}
+
+
